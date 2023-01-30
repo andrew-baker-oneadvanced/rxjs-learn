e59fb4a1d3f3c7d2c01245003bb276433545f074
Andrew Baker <andrew.baker@oneadvanced.com>
2023-01-30 16:32:57 +0000

    Hot vs. cold observables

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+/*
+import { Observable, share } from 'rxjs';
 
 
 /* const observable = Observable.create((observer: any) => {
@@ -12,17 +13,19 @@ import { Observable } from 'rxjs';
     observer.error(err);
   }
 }); */
+/*
 const observable = Observable.create((observer: any) => {
   try {
     observer.next('Hey guys!');
     observer.next('How are you?');
     setInterval(() => {
       observer.next('I am good');
-    }, 1000 * 2 /* seconds */);
+    }, 1000 * 2);  // 2 seconds
   } catch (err) {
     observer.error(err);
   }
-});
+// });
+}).pipe(share());
 
 const observer = observable.subscribe(
   (x: any) => addItem(x),
@@ -30,16 +33,22 @@ const observer = observable.subscribe(
   () => addItem('Completed')
 );
 
-const observer2 = observable.subscribe(
-  (x: any) => addItem(x)
-);
+setTimeout(() => {
+  const observer2 = observable.subscribe(
+    (x: any) => addItem('Subscriber 2: ' + x)
+  );
+}, 1000 * 6);  // 6 seconds
+*/
+import { fromEvent, Observable } from 'rxjs';
 
-observer.add(observer2);
-// observer.remove(observer2);
+
+const observable = fromEvent(document, 'mousemove');
 
 setTimeout(() => {
-  observer.unsubscribe();
-}, 1000 * 6 /* seconds */);
+  const subscription = observable.subscribe(
+    (x: any) => addItem(x)
+  );
+}, 1000 * 2);  // 2 seconds
 
 function addItem(val: any) {
   const node = document.createElement('li');
